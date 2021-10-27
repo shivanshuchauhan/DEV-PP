1c1315a58260616064e7f7c1d13cedc527b62e5c
Shivanshu Chauhan <shivanshuchauhan3004@gmail.com>
2021-10-27 22:39:39 +0530

    router

--- Module3_React/routingdemo/src/Components/Header.jsx
+++ Module3_React/routingdemo/src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class Header extends React.Component {
     state = {};
---
+++ Module3_React/routingdemo/src/Components/Settings.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Settings extends React.Component {
+    render() { 
+        return (
+            <h1>Home component</h1>
+        );
+    }
+}
+ 
+export default Settings;
